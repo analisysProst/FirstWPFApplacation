51395df14c40ac6df989f71a813eb74943fcacae
elkros.prostor@gmail.com <elkros.prostor@gmail.com>
2021-09-03 14:49:20 +0300

    пространства имен

---
+++ WpfApp1/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Смартфон {this.Name}; цена: {this.Price}";
+        }
+    }
+}
